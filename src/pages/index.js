aeec0406dc153d2c6e0b5bbf00e02b7888d83e63
osakepanda <yonayona.kyoto@gmail.com>
2021-06-23 11:05:12 +0900

    fix index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,50 +36,9 @@ class Index extends React.Component {
     const { allPrismicIndex } = data
 
     const {
-      url1_name
+      url1_title
     } = allPrismicIndex.edges[0].node.data
-
-    // const meta = [{
-    //   property: 'og:image:width',
-    //   content: seo_icon.dimensions.width,
-    // }, {
-    //   property: 'og:image:height',
-    //   content: seo_icon.dimensions.height,
-    // }]
-
-    return (
-      <>
-        {/*<SEO*/}
-        {/*  lang={lang}*/}
-        {/*  title={seo_title}*/}
-        {/*  page={baseline.text}*/}
-        {/*  description={seo_description}*/}
-        {/*  author={seo_author}*/}
-        {/*  thumbnail={seo_icon.url}*/}
-        {/*  meta={meta} />*/}
-
-        <header>
-          {/*<picture style={{ backgroundImage: `url(${cover.url})` }}></picture>*/}
-          <hgroup>
-            {/*<h1 style={{ backgroundImage: `url(${url1_name.text})` }}>{ url1_name.text }</h1>*/}
-            <h2>{ url1_name[0].text }</h2>
-            {/*<h3><span role="img" aria-label="time">🕖</span>{ opening.text }</h3>*/}
-            {/*<address><span role="img" aria-label="location">📍</span>{ address.text }</address>*/}
-            {/*<phone><span role="img" aria-label="phone">📞</span><a href={`tel:${phone}`}>{ phone }</a></phone>*/}
-          </hgroup>
-        </header>
-
-        {/*<section dangerouslySetInnerHTML={{ __html: content.html }}></section>*/}
-
-        {/*<aside>*/}
-        {/*  { posts.map((post, index) => <Post key={index} {...post}  />) }*/}
-        {/*</aside>*/}
-
-        {/*<iframe src="https://maps.google.com/maps?q=panda%20sake%20bar&z=13&output=embed" title="maps"></iframe>*/}
-      
-        <footer>&copy;2018 – made with <span role="img" aria-label="fuel">🍙</span> by <a href="https://instagram.com/y_nk">@y_nk</a></footer>
-      </>
-    )
+    return <h1>Hello, World!</h1>
   }
 }
 
