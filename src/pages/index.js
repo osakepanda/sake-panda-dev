77d2f1bb00a19aa31dbbaf6eee387518c03b7ab9
osakepanda <yonayona.kyoto@gmail.com>
2021-06-24 10:12:21 +0900

    fix index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,12 @@
 import React from "react";
 import { Link } from "gatsby";
-import Layout from "../components/layout";
-import Image from "../components/image";
-import SEO from "../components/seo";
 //import "./index.css";
 const IndexPage = () => (
-    <Layout>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <div className="home">
-        <h1>Hello There</h1>
-        <p>Welcome my awesome blog</p>
-        <div>
-          <div
-              style={{
-                maxWidth: `300px`,
-                margin: "0 auto 1.45rem"
-              }}
-          >
-            <Image />
-          </div>
-        </div>
-        <Link to="/sakepanda/">View all posts</Link>
-      </div>
-    </Layout>
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
 );
 
 export default IndexPage;
